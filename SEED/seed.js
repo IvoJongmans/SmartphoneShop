b84e743b0ac1b9d3745a40c8ece5139b4c846986
IvoJongmans <ivojongmans@gmail.com>
2019-10-18 13:18:49 +0200

    added verify to checkout page

--- SEED/seed.js
+++ SEED/seed.js
@@ -11,21 +11,19 @@ var phones = []
 const brands = ["Apple", "Samsung", "Nokia", "Alcatel", "Sony", "Huawei", "Xiaomi", "Honor", "ASUS", "Lenovo"]
 const models = ["X", " Galaxy S10", "3310", "P90", "Gorilla 5", "Redmi", "Switch 3", "Pro 2", "Gova", "Reyo"]
 const colors = ["black", "white", "pink", "orange", "yellow", "green", "blue", "silver", "red", "purple"]
-const prices = [99, 199, 299, 399, 499, 599, 699, 799, 899, 999]
 const images = ["apple.jpg", "samsung.jpg", "nokia.jpg", "alcatel.jpg", "sony.jpg", "huawei.jpg", "xiaomi.jpg", "honor.jpg", "asus.jpg", "lenovo.jpg"]
 
 for (i = 0; i < chooseSeedAmount; i++) {
     var randomBrand = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
     var randomModel = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
     var randomColor = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
-    var randomPrice = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
     var randomImage = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
 
     var phone = {
         brand: brands[randomBrand],
         model: models[randomModel],
         color: colors[randomColor],
-        price: prices[randomPrice],
+        price: Math.floor(Math.random() * (999 - 0 + 99)) + 100,
         image: images[randomImage],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
